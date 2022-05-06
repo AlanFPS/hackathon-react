0cc71352618f7a22a0a4853b7ecddf2f710b945f
Alan <alan_ps@hotmail.com>
2022-05-06 17:51:55 -0400

    IT OWRKS NOW

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import "./App.css";
-// import Signup from "./components/Signup";
+import Signup from "./components/Signup";
 import Checklist from "./components/Checklist";
-import { Routes, Route, Link } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <div>
-//       <nav>
-//         <h1>Sign Up</h1>
-//         <Signup />
-//         <br />
-//         {/* <nav> */}
-//         <h1>To-do List</h1>
-//         {/* </nav> */}
-//         <Checklist />
-//       </nav>
-//     </div>
-//   );
-// }
+// import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -25,23 +9,34 @@ function App() {
       <header>
         <p>TO DO LIST MANAGER</p>
         <nav>
-          <Link to="/">Go Home</Link>
+          <a href="/">Go Home</a>
           {/* <Link to="/colors">Go to colors</Link> */}
           {/* <Link to="/count">Go to Count</Link> */}
           {/* <Link to="/signup">Go to Signup</Link> */}
         </nav>
       </header>
+      <div>
+        <nav>
+          <h1>Sign Up</h1>
+          <Signup />
+          <br />
+          {/* <nav> */}
+          <h1>To-do List</h1>
+          {/* </nav> */}
+          <Checklist />
+        </nav>
+      </div>
 
       {/* npm i react-router-dom */}
-      <Routes>
-        {/* <Route path="/" element={<Hover />} /> */}
-        {/* <Route path="/greet/:name" element={<Greeting />} /> */}
-        {/* <Route path="/signup" element={<Signup />} /> */}
-        {/* <Route path="/count" element={<Count />} /> */}
-        <Route path="/colors" element={<Checklist />} />
-        {/* <Route path="/profile" element={<Profile />} /> */}
-        {/* <Route path="/*" element={<NotFound />} /> */}
-      </Routes>
+      {/* <Routes> */}
+      {/* <Route path="/" element={<Hover />} /> */}
+      {/* <Route path="/greet/:name" element={<Greeting />} /> */}
+      {/* <Route path="/signup" element={<Signup />} /> */}
+      {/* <Route path="/count" element={<Count />} /> */}
+      {/* <Route path="/colors" element={<Checklist />} /> */}
+      {/* <Route path="/profile" element={<Profile />} /> */}
+      {/* <Route path="/*" element={<NotFound />} /> */}
+      {/* </Routes> */}
     </div>
   );
 }
